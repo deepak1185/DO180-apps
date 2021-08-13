35a2fc99be8492ab397b8855dd6b48d3fc9bead2
Student User <student@workstation.lab.example.com>
2021-08-13 03:14:50 -0400

    new message

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Change is the only constant! So go with it.\n";
+print "Yesterdays excellence is todays mediocrity! So change.\n";
 ?>
