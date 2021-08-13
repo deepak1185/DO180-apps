3c69d81c234dfcc96606fab7a83ec40a0e341bb0
deepak1185 <59957064+deepak1185@users.noreply.github.com>
2021-08-13 14:55:31 +0900

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is required at all times" "\n";
-print "Change is the only constant" "\n";
+print "A change is required at all times \n";
+print "Change is the only constant \n";
 ?>
